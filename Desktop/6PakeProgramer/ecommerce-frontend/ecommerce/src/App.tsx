19c7eb05d155fb34393d803cc61b6ae3061f656a
Manmay <manmay3355@gmail.com>
2024-11-29 10:26:26 +0530

    add some feature

--- Desktop/6PakeProgramer/ecommerce-frontend/ecommerce/src/App.tsx
+++ Desktop/6PakeProgramer/ecommerce-frontend/ecommerce/src/App.tsx
@@ -1,12 +1,37 @@
 
-import './App.css'
+import { BrowserRouter as Router , Routes , Route} from "react-router-dom"
+import { FaSearch, FaCartPlus} from 'react-icons/fa'
+import {lazy, Suspense } from "react"
+import Loader from "./component/Loader";
+
+
+
+const Home = lazy(()=>import ('./pages/Home'));
+const Search = lazy(()=>import ('./pages/Search'));
+const Cart = lazy(()=>import ('./pages/Cart'));
+
 
 function App() {
 
   return (
-    <>
-     
-    </>
+     <Router>
+
+
+
+          {/* Header */}
+
+      <Suspense fallback={<Loader/>}>
+      <Routes>
+
+        <Route path="/" element={<Home/>} />
+        <Route path="/search" element={<Search/>} />
+        <Route path="/search" element={<Cart/>}/>
+
+      </Routes>
+
+      </Suspense>
+     </Router>
+
   )
 }
 
