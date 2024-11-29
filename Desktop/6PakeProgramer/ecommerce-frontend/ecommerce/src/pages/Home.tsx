7cf1182a486561b28415532d3c177350605e8b59
Manmay <manmay3355@gmail.com>
2024-11-29 13:08:10 +0530

    Home

--- Desktop/6PakeProgramer/ecommerce-frontend/ecommerce/src/pages/Home.tsx
+++ Desktop/6PakeProgramer/ecommerce-frontend/ecommerce/src/pages/Home.tsx
@@ -1,10 +1,33 @@
-
+import { Link } from "react-router-dom"
+import ProductCard from "../components/ProductCard"
 
 
 
 const Home = () => {
+
+  const addToCartHandler =()=>{};
   return (
-    <div>Home</div> 
+    <div className="home">
+      
+      <section></section>
+
+      <h1>Latest Product
+        <Link to="/search" className="findmore">More</Link>
+      </h1>
+
+      <main>
+
+
+        <ProductCard 
+        productId="addddd" 
+        name="camera" 
+        price={49} 
+        stock={5}
+        handler={addToCartHandler}
+        photo="https://m.media-amazon.com/images/I/61xl4inLS5L._AC_UY327_FMwebp_QL65_.jpg"
+        />
+      </main>
+    </div> 
   )
 }
 
