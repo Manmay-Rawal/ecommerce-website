638079467ab4a22b5f321959985bcedb73835b7f
Manmay <manmay3355@gmail.com>
2024-11-30 09:18:00 +0530

    backend first commit

---
+++ Desktop/6PakeProgramer/ecommerce-backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app =express();
+
+
+// middleware
+app.use(express.json())
+
+
+// APIs
+// app.get("/",(req:Request,res:Response)=>{res.send({message:"server is start"}))};
+// app.get("/", (req: Request, res: Response) => {
+//     res.json({ message: "server is start" });
+//   });
+// app.get("/",(req,res)=>res.send({message:"Server is start hii  "}))
+
+
+//listain
+const PORT=4000;
+const localhost="ecommerce"
+
+
+app.listen(PORT,()=>{
+    console.log(`express is start on http://${localhost}:${PORT}`);
+    
+})
